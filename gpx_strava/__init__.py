02e35790f0bdbbc4155badf4c5443578267e59cd
Safwen Bouali <safwen.bouali@outlook.com>
2018-09-29 02:09:48 +0100

    Update __init__.py
    formating in an object oriented manner. 
    usable 'activity' object and other methods.
    few bugs with the stitching part, will be fixed, and then added to the code very soon.

--- gpx_strava/__init__.py
+++ gpx_strava/__init__.py
@@ -1,98 +1,91 @@
 name = "gpx_strava"
 
-from bs4 import BeautifulSoup
-from datetime import datetime
+import xml.etree.ElementTree as ET
+from datetime import datetime,timedelta
 import dateutil.parser
-
-__xml_header = """<?xml version="1.0" encoding="UTF-8"?>"""
-__gpx_header = """<gpx creator="StravaGPX Android" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">"""
-__metadata_header = """<metadata><time>%s</time></metadata>"""
-__trk_header = """<trk><name>%s</name><type>1</type><trkseg>"""
-__gpx_footer = """</trkseg></trk></gpx>"""
+import xml.dom.minidom 
 
 
-def datetime_converter(timestamp,to_string=True):
-    """if the 'to_string' variable is true, it converts a datetime object
-    to a string in the ISO 8601 format with a 'T' as a delimiter.
-    else, to converts a string coded in ISO 8601 format to a datetime object."""
-    try:
+
+class activity:
+    """An activity object with all the necessary operations"""
+    
+    
+    def __datetime_converter(timestamp,to_string=True):
+        """if the 'to_string' variable is true, it converts a datetime object
+        to a string in the ISO 8601 format with a 'T' as a delimiter.
+        else, to converts a string coded in ISO 8601 format to a datetime object."""
         if (to_string):
             return timestamp.isoformat('T')
         else:
             return dateutil.parser.parse(timestamp)
-    except e:
-        print('wrong variable type! ' + e)
+
+        
+    def __parse_gpx_string(data_source,from_file=True):
+
+        if(from_file):
+            _trk_seg = ET.parse(data_source).getroot()[0][0]
+        else:
+            _trk_seg = ET.fromstring(data_source).getroot()[0][0]
+        trk_pts = []
+        first_stamp = ''
+        is_first = True
+        for trk_pt in _trk_seg:
+            temp_pt = activity.__parse_trkpt(trk_pt,is_first)
+            if(is_first):
+                first_stamp = temp_pt[3]
+                temp_pt[3] = 0
+                is_first = False
+            else:
+                temp_pt[3] = (temp_pt[3] - first_stamp).total_seconds()
+            trk_pts.append(temp_pt)
+        return (first_stamp,trk_pts)
 
 
-def create_trkpt(trkpt):
-    """
-    """
-    #[timestamp,[lat,long,elevation]]
-    _long = trkpt[1][0]
-    _lat = trkpt[1][1]
-    _elev = trkpt[1][2]
-    _time = trkpt[0]
-    return """<trkpt lat=\"""" + str(_lat) + """\" lon=\"""" + str(_long) + """\"><ele>""" + str(_elev) + """</ele><time>""" + datetime_converter(timestamp) + """</time></trkpt>"""
-
-def create_gpx_string(name,trkpts):
-
-    res = __xml_header + __gpx_header + (__metadata_header % datetime_converter(trkpts[0][0])) + (__trk_header % name)
-    for trkpt in trkpts:
-        res += create_trkpt(trkpt)
-    res += __gpx_footer
-    return res
+    def __parse_trkpt(trk_pt,is_first):
+        _latitude = trk_pt.attrib['lat']
+        _longitude = trk_pt.attrib['lon']
+        _elevation = trk_pt[0].text
+        _timestamp = datetime_converter(trk_pt[1].text,to_string=False)
+        return [_latitude,_longitude,_elevation,_timestamp]
 
 
-def create_file(name,string_data,path=''):
+    def __encode_gpx_xml(data_source):
 
-    f = open(path + name, 'w')
-    f.write(string_data)
-    f.close()
+        trk_pts = data_source[1]
+        first_stamp = data_source[0]
+        is_first = True
+        gpx = ET.Element('gpx',attrib={'creator':'StravaGPX'})
+        trk = ET.SubElement(gpx,'trk')
+        trkseg = ET.SubElement(trk,'trkseg')
+        for trk_pt in trk_pts:
+            trkseg.append(activity.__encode_trkpt(trk_pt,first_stamp))
+        return gpx
 
+    
+    def __encode_trkpt(trk_pt,first_stamp):
 
-def parse_gpx_string(data):
+        temp_pt = ET.Element('trkpt',attrib={'lat':trk_pt[0],'lon':trk_pt[1]})
+        _ele = ET.SubElement(temp_pt, 'ele')
+        _ele.text = trk_pt[2]
+        _time = ET.SubElement(temp_pt, 'time')
+        _time.text = datetime_converter(first_stamp + timedelta(seconds = trk_pt[3]))
+        return temp_pt
 
-    soup = BeautifulSoup(data, 'xml.parser')
-    res = []
-    for trkpt in soup.find_all('trkpt'):
-        _lat = float(trkpt.trkpt['lat'][0])
-        _lon = float(trkpt.trkpt['lon'][0])
-        _elev = float(trkpt.find('ele').get_text())
-        _timestamp = datetime_converter(trkpt.find('time').get_text(), to_string=False)
-        res.append([_timestamp,[_lat,_lon,_elev]])
-    return res
+    
+    def __init__(self,data_source,from_file=True):
 
-
-def gpx_data_to_incremental_stamps(data):
-
-    res = []
-    first_stamp = data[0][0]
-    for trkpt in data:
-        _ts = (trkpt[0] - first_stamp).seconds
-        res.append([_ts,trkpt[1]])
-    return res
-
-
-def incremental_stamps_to_csv(incremental_stamps,file_name):
-
-    import pandas as pd
-    columns = ['timestamp','lat','long','elev']
-    data = []
-    for trkpt in incremental_stamps:
-        elem = []
-        elem.append(trkpt[0])
-        elem.append(trkpt[1][0])
-        elem.append(trkpt[1][1])
-        elem.append(trkpt[1][2])
-        data.append(elem)
-    df = pd.DataFrame(data,columns=columns)
-    df.to_csv(file_name)
-    return df
-
-
-def __read_file(file):
-
-    f = open(file)
-    data_str = f.read()
-    f.close()
-    return data_str
+        self.first_stamp,self.trk_points = activity.__parse_gpx_string(data_source,from_file=from_file)
+        
+    def to_xml(self):
+    
+        return activity.__encode_gpx_xml((self.first_stamp,self.trk_points))
+    
+    
+    def __str__(self):
+        
+        return ET.tostring(self.to_xml(), 'unicode', method="xml")
+    
+    def to_file(self,name):
+        
+        ET.ElementTree(self.to_xml()).write(name + '.gpx')
