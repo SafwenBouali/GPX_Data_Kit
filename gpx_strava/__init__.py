7c3905a099ac6cab7835460613ab555fef64a736
Safwen Bouali <safwen.bouali@outlook.com>
2019-04-13 02:22:37 +0100

    Update
    second version of the kit, supporting TrkPointExtensions and better code aesthetics when dealing with xml.

--- gpx_strava/__init__.py
+++ gpx_strava/__init__.py
@@ -3,13 +3,14 @@ name = "gpx_strava"
 import xml.etree.ElementTree as ET
 from datetime import datetime,timedelta
 import dateutil.parser
-import xml.dom.minidom 
-
+import xml.dom.minidom
+import random
+import geopy.distance
+from xml.dom import minidom
 
 
 class activity:
     """An activity object with all the necessary operations"""
-    
     
     def __datetime_converter(timestamp,to_string=True):
         """if the 'to_string' variable is true, it converts a datetime object
@@ -21,110 +22,115 @@ class activity:
         else:
             return dateutil.parser.parse(timestamp)
 
+    def __find_by_tag_name(element,tag_name):
         
-    def __parse_gpx(data_source,from_file=True):
-        """given gpx data, a file or a string, it parses the data
+        for elmnt in element:
+            if (elmnt.tag.endswith(tag_name)):
+                return elmnt
+        print("no " + tag_name + " node found")
+        return
+    
+    def __compare_speed(pt_1,pt_2,speed):
+        print('not implemented')
+    
+    def __parse_gpx(data_source):
+        """given gpx data, a string, it parses the data
         into an array of points"""
         
-        if(from_file):
-            _gpx = ET.parse(data_source).getroot()
-        else:
-            _gpx = ET.fromstring(data_source)
-        _trk_seg = _gpx[1][2]
+        _gpx = ET.fromstring(data_source)
+        ET.register_namespace('gpxtpx',"http://www.garmin.com/xmlschemas/TrackPointExtension/v1") 
+        
+        
+        _root_node = ET.Element('gpx')
+        
+        _trk_seg = activity.__find_by_tag_name(activity.__find_by_tag_name(_gpx,'trk'),'trkseg')
+        
         trk_pts = []
+        
         first_stamp = ''
+        
         is_first = True
+        
+        temp_pt = None
+        temp_time = None
+        
         for trk_pt in _trk_seg:
-            temp_pt = activity.__parse_trkpt(trk_pt,is_first)
+            temp_pt = trk_pt
+            temp_time = activity.__find_by_tag_name(temp_pt,'time')
             if(is_first):
-                first_stamp = temp_pt[3]
-                temp_pt[3] = 0
+                first_stamp = activity.__datetime_converter(temp_time.text,False)
+                temp_time.text = 0
                 is_first = False
             else:
-                temp_pt[3] = (temp_pt[3] - first_stamp).total_seconds()
+                temp_time.text = (activity.__datetime_converter(temp_time.text,False) - first_stamp).total_seconds()
             trk_pts.append(temp_pt)
-        return (first_stamp,trk_pts)
-
-
-    def __parse_trkpt(trk_pt,is_first):
-        """parses each point into an array, compromised of:
-        lat, long, elevation and a timestamp"""
-        
-        _latitude = trk_pt.attrib['lat']
-        _longitude = trk_pt.attrib['lon']
-        _elevation = trk_pt[0].text
-        _timestamp = activity.__datetime_converter(trk_pt[1].text,to_string=False)
-        return [_latitude,_longitude,_elevation,_timestamp]
-
-
-    def __encode_gpx_xml(data_source):
-        """encodes the array of points to an xml ET"""
-        
-        trk_pts = data_source[1]
-        first_stamp = data_source[0]
-        is_first = True
-        gpx = ET.Element('gpx',attrib={'creator':'StravaGPX'})
-        trk = ET.SubElement(gpx,'trk')
-        trkseg = ET.SubElement(trk,'trkseg')
-        for trk_pt in trk_pts:
-            trkseg.append(activity.__encode_trkpt(trk_pt,first_stamp))
-        return gpx
-
+        return (first_stamp,trk_pts,_root_node,temp_time.text)
     
-    def __encode_trkpt(trk_pt,first_stamp):
-        """encodes a point into an XML element"""
-        
-        temp_pt = ET.Element('trkpt',attrib={'lat':trk_pt[0],'lon':trk_pt[1]})
-        _ele = ET.SubElement(temp_pt, 'ele')
-        _ele.text = trk_pt[2]
-        _time = ET.SubElement(temp_pt, 'time')
-        _time.text = activity.__datetime_converter(first_stamp + timedelta(seconds = trk_pt[3]))
-        return temp_pt
-
-    
-    def __init__(self,data_source,data_type='file'):
-        
-        if(data_type is 'file'):
-            self.first_stamp,self.trk_points = activity.__parse_gpx(data_source)
-        elif(data_type is 'str'):
-            self.first_stamp,self.trk_points = activity.__parse_gpx(data_source,from_file=False)
-        elif(data_type is 'ready'):
-            self.first_stamp,self.trk_points = data_source
-        
-    def to_xml(self):
-    
-        return activity.__encode_gpx_xml((self.first_stamp,self.trk_points))
-    
-    
-    def __str__(self):
-        
-        return ET.tostring(self.to_xml(), 'unicode', method="xml")
-    
-    def to_file(self,name=''):
-        
+    def add_seconds(self,seconds_to_add):
+        for point in self.trk_points:
+            temp_pt = point
+            temp_time = activity.__find_by_tag_name(temp_pt,'time') 
+            temp_time.text += seconds_to_add
+            
+    def to_file(self, name=''):
+        self.build_xml()
         if(name is ''):
-            name = "stitched_activity"
+            name = 'activity '+str(datetime.now()).replace(' ','').replace('-','').replace(':','')
         name += ".gpx"
-        ET.ElementTree(self.to_xml()).write(file_or_filename=name)
-        
-    def add_seconds(self,seconds):
-        
-        for pt in self.trk_points:
-            pt[3] += seconds
-        
-    def get_last_trk_seconds_count(self):
-        
-        return self.trk_points[-1][3]
+        tree = ET.ElementTree(self.__root_node)
+        tree.write(name,encoding='UTF-8', method="xml")
+    
+    def build_xml(self):
+        trk = ET.SubElement(self.__root_node,'trk')
+        trkseg = ET.Element('trkseg')
+        for point in self.trk_points:
+            temp_time = activity.__find_by_tag_name(point,'time')
+            temp_time.text = activity.__datetime_converter(self.first_stamp + timedelta(seconds = temp_time.text))
+            trkseg.append(point)
+        trk.append(trkseg)
+        return self.__root_node
     
     
+    def __init__(self,data_source,data_type='file',crop_threshold_speed=-1):
+        
+        
+        data = None
+        
+        if (data_type is 'ready'):
+            data = data_source
+        else:
+            if (data_type is 'file'):
+                data = open(data_source).read()
+            else:
+                data = data_source
+            data = activity.__parse_gpx(data.replace('xmlns="http://www.topografix.com/GPX/1/1"', '', 1))
+            
+        self.first_stamp,self.trk_points,self.__root_node,self.__duration = data
+        if (crop_threshold_speed is not -1):
+            if (crop_threshold_speed < 1):
+                print("speed has to be more than 1 km/h")
+            else:
+                self.crop(crop_threshold_speed)
+        
+    
+    def crop(self,speed_threshold=10.0):
+        print('not implemented')
+    
+    
+    def stitch(activities):
 
-def stitch_activities(activities):
-    """stitches activities into one activity without time lapses in-between.
-    assumes that activities has a length of at least 2"""
+        first_stamp = activities[0].first_stamp
+        root_node = activities[0].__root_node
+        trk_points = []
+        last_total = 0
+        for i in range(len(activities)):
+            activities[i].add_seconds(last_total)
+            trk_points.extend(activities[i].trk_points)
+            last_total += activities[i].__duration
+        return activity((first_stamp,trk_points,root_node,last_total),data_type="ready")
+
+
+
+    def join(activities):
+        print('not implemented')
     
-    first_stamp = activities[0].first_stamp
-    combined_trk_points = activities[0].trk_points
-    for i in range(1,len(activities)):
-        activities[i].add_seconds(activities[i-1].get_last_trk_seconds_count())
-        combined_trk_points.extend(activities[i].trk_points[1:])
-    return activity((first_stamp,combined_trk_points),data_type='ready')
